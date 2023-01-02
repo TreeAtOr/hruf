e00471accf3f9eda51c6709cf9a10a4bc0ae9ca0
Vlad Glazunov <v.glazunov@global-system.ru>
2023-01-02 18:13:35 +0300

    chore: init board service

---
+++ services/board/src/test/java/com/hruf/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hruf.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
